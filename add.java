cbfea71a1d68747051191f7767c4380e9811bc98
Arun0227 <119296524+Arun0227@users.noreply.github.com>
2022-12-01 00:08:24 +0530

    java 8 program added

--- add.java
+++ add.java
@@ -15,3 +15,16 @@ public class joes {//avg and sum
 		System.out.println("avg of"+sum/n);
 	}
 }
+
+//temp file add java 8
+
+import java.util.*;
+import java.util.stream.*;
+public class JavaHungry {
+    public static void main(String args[]) {
+            List<Integer> myList = Arrays.asList(10,15,8,49,25,98,32);
+            myList.stream()
+                  .filter(n -> n%2 == 0)
+                  .forEach(System.out::println);
+    }
+}
